e9be2a08f25364123964db5f08b41a468d33eb9b
svaronc <sebastianvaron96@gmail.com>
2023-11-14 23:58:58 +0000

    create a get reuqest to render the tweets already created on route /tweets and use timeago library to display the time

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,65 +3,59 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+const renderTweets = function (tweets) {
+  for (const tw of tweets) {
+    const $tweet = createTweetElement(tw);
+    $("#tweets-container").append($tweet);
+  }
+};
+const createTweetElement = function (data) {
+  let tweetDay = timeago.format(data.created_at);
+  const $tweet = $(`
+<article>
+      <header class="tweet-header">
+        <p><i class="fa-solid fa-user"></i>${data.user.name}</p>
+        <p>${data.user.handle}</p>
+      </header>
+      <textarea disabled = "true" name="text" class="tweet-main-text">${data.content.text}</textarea>
+      <footer class="tweet-footer">
+        <p>${tweetDay}</p>
+        <div class="tweet-footer-icons">
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-heart"></i>
+        </div>
+      </footer>
+    </article>
+`);
+  return $tweet;
+};
+
+const loadTweets = function () {
+  $.ajax({
+    type: "GET",
+    url: "/tweets",
+    dataType: "json",
+    success: function (response) {
+      console.log("loggin succes --->", response);
+      renderTweets(response);
+    },
+  });
+};
 
 $(document).ready(function () {
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const data = [
-    {
-      user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac",
+  loadTweets();
+  $("#new-tweet-form").on("submit", function (event) {
+    event.preventDefault();
+    let serializeData = $(this).serialize();
+    console.log(serializeData);
+    $.ajax({
+      type: "POST",
+      url: "/tweets",
+      data: serializeData,
+      success: function (response) {
+        console.log(response);
       },
-      content: {
-        text: "If I have seen further it is by standing on the shoulders of giants",
-      },
-      created_at: 1461116232227,
-    },
-    {
-      user: {
-        name: "Descartes",
-        avatars: "https://i.imgur.com/nlhLi3I.png",
-        handle: "@rd",
-      },
-      content: {
-        text: "Je pense , donc je suis",
-      },
-      created_at: 1461113959088,
-    },
-  ];
-
-  // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-  const renderTweets = function (tweets) {
-    for (const tw of tweets) {
-      const $tweet = createTweetElement(tw);
-      $("#tweets-container").append($tweet);
-    }
-  };
-  const createTweetElement = function (data) {
-    let tweetDay = new Date(data.created_at);
-    const $tweet = $(`
-  <article>
-        <header class="tweet-header">
-          <p><i class="fa-solid fa-user"></i>${data.user.name}</p>
-          <p>${data.user.handle}</p>
-        </header>
-        <textarea disabled = "true" name="text" class="tweet-main-text">${
-          data.content.text
-        }</textarea>
-        <footer class="tweet-footer">
-          <p>${tweetDay.getDay()} days ago</p>
-          <div class="tweet-footer-icons">
-            <i class="fa-solid fa-retweet"></i>
-            <i class="fa-solid fa-flag"></i>
-            <i class="fa-solid fa-heart"></i>
-          </div>
-        </footer>
-      </article>
-  `);
-    return $tweet;
-  };
-  // Test / driver code (temporary)
-  renderTweets(data);
-  console.log($tweet); // to see what it looks like
+    });
+  });
 });
