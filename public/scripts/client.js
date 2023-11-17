e6126dae759ca059783b165ffbd5edf77d14de3f
svaronc <sebastianvaron96@gmail.com>
2023-11-17 00:07:58 +0000

    toggle new tweet section on click

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -74,4 +74,7 @@ $(document).ready(function () {
     event.preventDefault();
     submitNewTweet();
   });
+  $(".header-rigth").on("click", function () {
+    $(".new-tweet").toggle("slow");
+  });
 });
